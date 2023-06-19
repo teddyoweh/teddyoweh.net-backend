f9de76c2371c76de870c11443b120c5b4c26256c
Ifechukwudeni Oweh <teddyoweh@gmail.com>
2023-06-18 20:55:43 -0700

    Update views.module.ts

--- src/module/views.module.ts
+++ src/module/views.module.ts
@@ -1,21 +1,12 @@
 import { Module, OnModuleInit } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ViewController, ReqeustController } from '../controllers/views.controller';
+import { ViewController,ReqeustController } from '../controllers/views.controller';
 import { ViewService } from '../services/view.service';
 import { SocketGateway } from '../middlewares/socket.gateway';
 import { ViewsModel } from 'src/models/view.model';
-
 @Module({
-  imports: [
- 
-  ],
-  controllers: [ViewController, ReqeustController],
-  providers: [ViewService, SocketGateway],
+  imports: [],
+  controllers: [ViewController,ReqeustController],
+  providers: [ViewService,SocketGateway],
 })
-export class ViewsModule implements OnModuleInit {
-  constructor(private readonly socketGateway: SocketGateway) {}
-
-  async onModuleInit() {
-    await this.socketGateway.start();
-  }
-}
+export class ViewsModule {}
